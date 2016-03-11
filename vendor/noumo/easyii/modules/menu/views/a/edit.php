909b336b2100f0683061f1193e9e8d85ed891916
dn-dz <admin@persona.pp.ua>
2016-03-11 11:20:48 +0200

    DELETE ALL

--- vendor/noumo/easyii/modules/menu/views/a/edit.php
+++
@@ -1,5 +0,0 @@
-<?php
-$this->title = Yii::t('easyii/menu', 'Edit page');
-?>
-<?= $this->render('_menu') ?>
-<?= $this->render('_form', ['model' => $model]) ?>
--- vendor/noumo/easyii/modules/price/views/a/create.php
+++
@@ -1,5 +0,0 @@
-<?php
-$this->title = Yii::t('easyii/price', 'Create price');
-?>
-<?= $this->render('_menu') ?>
-<?= $this->render('_form', ['model' => $model]) ?>
